66e7e6bd31ea4d502466d1abbcdc9dfd78233a24
Jim Hribar <jhribar@onshift.com>
2017-06-06 14:21:48 -0400

    support no image

--- run.py
+++ run.py
@@ -9,26 +9,20 @@ app = Flask(__name__)
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Respond to incoming calls with a simple text message."""
-
-    # resp = MessagingResponse().message("Hello, Mobile Monkey")
-
     body = request.values.get('Body', None)
-    num_media = request.values.get('NumMedia', 0)
-    media_url_0 = None
-
-    if int(num_media) > 0:
-        media_url_0 = request.values.get('MediaUrl0', None)
-        # msg = msg.media(media_url_0)
-
-    # msg = Message().body("Hello, Mobile Monkey").media("https://demo.twilio.com/owl.png")
-    msg = Message().body(body).media(media_url_0)
-
+    num_media = int(request.values.get('NumMedia', 0))
+    msg = Message().body(body)
     resp = MessagingResponse()
+
+    # Only support a single picture
+    if num_media > 0:
+        media_url_0 = request.values.get('MediaUrl0', None)
+        msg = msg.media(media_url_0)
+
     resp.append(msg)
 
     return str(resp)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    # app.url_map.strict_slashes = False
     app.run(debug=True, host='0.0.0.0', port=port)
