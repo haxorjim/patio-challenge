5c349a7360e5a461b3445fea8f16069eaebd17b7
Jim Hribar <jhribar@onshift.com>
2017-06-06 14:46:48 -0400

    Save body and image to database

---
+++ post.sql
@@ -0,0 +1,6 @@
+CREATE TABLE post
+(
+    id serial primary key,
+    body text NULL,
+    media_url text NULL
+);
--- run.py
+++ run.py
@@ -1,9 +1,27 @@
 from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from twilio.twiml.messaging_response import MessagingResponse, Message
 import os
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    media_url = db.Column(db.Text)
+
+    def __init__(self, body, media_url):
+        self.body = body
+        self.media_url = media_url
+
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
 
 
 @app.route("/sms", methods=['GET', 'POST'])
@@ -16,11 +34,15 @@ def incoming_sms():
 
     if num_media == 1:
         media_url_0 = request.values.get('MediaUrl0', None)
+
+        db.session.add(Post(body, media_url_0))
+        db.session.commit()
+
         msg = msg.media(media_url_0)
     elif num_media > 1:
         msg = Message().body('Please only include one image.')
     else:
-        msg = Message().body('Please include an image with your message.')
+        msg = Message().body('Please include an image.')
 
     resp.append(msg)
 
@@ -28,4 +50,5 @@ def incoming_sms():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+
     app.run(debug=True, host='0.0.0.0', port=port)
